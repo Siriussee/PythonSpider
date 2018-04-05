1d94fdebaa74b04a728e12723b56d92ec87303dd
SiriusSee <SiriusSee@outlook.com>
2018-04-06 01:40:13 +0800

    mod

--- do_it_again_with_proxy.py
+++ do_it_again_with_proxy.py
@@ -1,26 +1,38 @@
 import urllib2
 import re
 import time 
+import random
 
 with open('url_of_papers_no_404.txt', 'r') as f:
     urls = f.read().split('\n')
 
+with open('proxy.txt', 'r') as f:
+    proxys = f.read().split('\n')
+
 user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
 headers = { 'User-Agent' : user_agent }
+
 for url in urls:
     try:
         try:print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) +' getting ' + url 
         except:pass
+        
+        index = random.randint(0,len(proxys)-2)
+        random_proxy = proxys[index]
+        proxy_obj = urllib2.ProxyHandler({'http': random_proxy})
+        opener = urllib2.build_opener(proxy_obj)
+        urllib2.install_opener(opener)
         request = urllib2.Request(url, headers = headers)
         response = urllib2.urlopen(request, timeout = 10)
-        # print response.read()
         html_text = response.read()
     except:
-        with open('log.txt', 'ab') as f:
+        with open('log_2.txt', 'ab') as f:
             f.write(
-                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + 
-                ' an error occurred when requesting *' + url + '\n'
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '#' +
+                url + '#' + random_proxy + '\n'
             )
+        if len(proxys)-2 > 100:
+            del proxys[index]
         continue
 
 
@@ -40,6 +52,6 @@ for url in urls:
         catagory = re.findall(pattern_catagory_in_lines,html_text)[0].split('>')[1].split('<')[0] if re.findall(pattern_catagory_in_lines,html_text) else 'null'
     # print catagory
 
-    with open('API_url_5.txt', 'ab') as f:
+    with open('API_url_6.txt', 'ab') as f:
         f.write('https://api.altmetric.com/v1/doi/'+ DOI + '#' + catagory +'\n')
 
--- get_proxy.py
+++ get_proxy.py
@@ -1,13 +1,7 @@
 import json
 import urllib2
 import re
-import requests
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+import time
 
 class JSONObject:
     def __init__(self, d):
@@ -23,8 +17,8 @@ try:
     request = urllib2.Request(url, headers=headers)
     response = urllib2.urlopen(request)
     html_text = response.read()
-except urllib2.URLError, e:
-    print e.reason
+except:
+    print 'error'
 
 proxys = html_text.split('\n')
 # '{"country": "TH", "response_time": 8.72, "anonymity": "high_anonymous", "export_address": ["61.91.235.226"], "host": "61.91.235.226", "type": "https", "port": 8080, "from": "txt"}'
@@ -40,38 +34,20 @@ for proxy in proxys:
     except:
         continue
 
-work_proxys_plain = []
+
 
 for proxy in proxys_plain:
     try:
-        requests.get('http://science.sciencemag.org/content/339/6115/43/tab-article-info',proxies={"http": "http://" + proxy}, timeout=2)
+        proxy_obj = urllib2.ProxyHandler({'http': proxy})
+        opener = urllib2.build_opener(proxy_obj)
+        urllib2.install_opener(opener)
+        request = urllib2.Request(url, headers = headers)
+        response = urllib2.urlopen(request, timeout = 5)
     except:
+        try:print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' error ' + proxy + '\n'
+        except:pass
         continue
-    else:
-        try:
-            chrome_options = webdriver.ChromeOptions()
-            chrome_options.add_argument('--proxy-server=http://' + proxy)
-            chrome_options.add_argument('--ignore-certificate-errors')
-            chrome_options.add_argument('--ignore-ssl-errors')
-            browser = webdriver.Chrome(chrome_options=chrome_options)
-            browser.set_page_load_timeout(30)
-            browser.get('http://science.sciencemag.org/content/339/6115/43/tab-article-info')
-            element = WebDriverWait(browser, 30).until(
-                EC.presence_of_element_located((By.ID, "_altmetric_popover_el"))
-            )
-        except:
-            try:print 'failed'
-            except:pass
-            browser.close()
-            continue
-        else:
-            try:print 'good'
-            except:pass
-            work_proxys_plain.append(proxy)
-            with open('good_proxy_2.txt', 'ab') as f:
-                f.write(proxy+'\n')
-        browser.close()
-
-with open('proxy.txt', 'ab') as f:
-    for proxy in work_proxys_plain:
-        f.write(proxy+'\n')
+    with open('proxy_2.txt', 'ab') as f:
+        try:print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + ' pass ' + proxy + '\n'
+        except:pass
+        f.write(proxy + '\n')
